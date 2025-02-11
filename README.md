b37e43b58a2c5bf17f848d903b6157ac35742930
valeratrades <valeratrades@gmail.com>
2025-02-11 15:15:56 +0000

    -- New Project Snippet --

---
+++ README.md
@@ -0,0 +1,41 @@
+# postx
+![Minimum Supported Rust Version](https://img.shields.io/badge/nightly-1.86+-ab6000.svg)
+[<img alt="crates.io" src="https://img.shields.io/crates/v/postx.svg?color=fc8d62&logo=rust" height="20" style=flat-square>](https://crates.io/crates/postx)
+[<img alt="docs.rs" src="https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logo=docs.rs&style=flat-square" height="20">](https://docs.rs/postx)
+![Lines Of Code](https://img.shields.io/badge/LoC-0-lightblue)
+<br>
+[<img alt="ci errors" src="https://img.shields.io/github/actions/workflow/status/valeratrades/postx/errors.yml?branch=master&style=for-the-badge&style=flat-square&label=errors&labelColor=420d09" height="20">](https://github.com/valeratrades/postx/actions?query=branch%3Amaster) <!--NB: Won't find it if repo is private-->
+[<img alt="ci warnings" src="https://img.shields.io/github/actions/workflow/status/valeratrades/postx/warnings.yml?branch=master&style=for-the-badge&style=flat-square&label=warnings&labelColor=d16002" height="20">](https://github.com/valeratrades/postx/actions?query=branch%3Amaster) <!--NB: Won't find it if repo is private-->
+
+<!-- markdownlint-disable -->
+<details>
+  <summary>
+    <h2>Installation</h2>
+  </summary>
+	<pre><code class="language-sh">TODO</code></pre>
+</details>
+<!-- markdownlint-restore -->
+
+## Usage
+TODO
+
+<br>
+
+<sup>
+	This repository follows <a href="https://github.com/valeratrades/.github/tree/master/best_practices">my best practices</a> and <a href="https://github.com/tigerbeetle/tigerbeetle/blob/main/docs/TIGER_STYLE.md">Tiger Style</a> (except "proper capitalization for acronyms": (VsrState, not VSRState) and formatting).
+</sup>
+
+#### License
+
+<sup>
+Licensed under either of <a href="LICENSE-APACHE">Apache License, Version
+2.0</a> or <a href="LICENSE-MIT">MIT license</a> at your option.
+</sup>
+
+<br>
+
+<sub>
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in this crate by you, as defined in the Apache-2.0 license, shall
+be dual licensed as above, without any additional terms or conditions.
+</sub>
---
+++ flake.nix
@@ -0,0 +1,96 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
+    workflow-parts.url = "github:valeratrades/.github?dir=.github/workflows/nix-parts";
+    hooks.url = "github:valeratrades/.github?dir=hooks";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      pre-commit-hooks,
+      workflow-parts,
+      hooks,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = builtins.trace "flake.nix sourced" [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              treefmt = {
+                enable = true;
+                settings = {
+                  #BUG: this option does NOTHING
+                  fail-on-change = false; # that's GHA's job, pre-commit hooks stricty *do*
+                  formatters = with pkgs; [
+                    nixpkgs-fmt
+                  ];
+                };
+              };
+            };
+          };
+        };
+        workflowContents = (import ./.github/workflows/ci.nix) { inherit pkgs workflow-parts; };
+      in
+      {
+        packages =
+          let
+            manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+            rust = (pkgs.rust-bin.fromRustupToolchainFile ./.cargo/rust-toolchain.toml);
+            rustc = rust;
+            cargo = rust;
+            stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
+            rustPlatform = pkgs.makeRustPlatform {
+              inherit rustc cargo stdenv;
+            };
+          in
+          {
+            default = rustPlatform.buildRustPackage rec {
+              pname = manifest.name;
+              version = manifest.version;
+
+              buildInputs = with pkgs; [
+                openssl.dev
+              ];
+              nativeBuildInputs = with pkgs; [ pkg-config ];
+
+              cargoLock.lockFile = ./Cargo.lock;
+              src = pkgs.lib.cleanSource ./.;
+            };
+          };
+
+        devShells.default =
+          with pkgs;
+          mkShell {
+            inherit stdenv;
+            shellHook =
+              checks.pre-commit-check.shellHook
+              + ''
+                rm -f ./.github/workflows/errors.yml; cp ${workflowContents.errors} ./.github/workflows/errors.yml
+                rm -f ./.github/workflows/warnings.yml; cp ${workflowContents.warnings} ./.github/workflows/warnings.yml
+
+                cargo -Zscript -q ${hooks.appendCustom} ./.git/hooks/pre-commit
+                cp -f ${(import hooks.treefmt { inherit pkgs; })} ./.treefmt.toml
+              '';
+            packages = [
+              mold-wrapped
+              openssl
+              pkg-config
+              (rust-bin.fromRustupToolchainFile ./.cargo/rust-toolchain.toml)
+            ] ++ checks.pre-commit-check.enabledPackages;
+          };
+      }
+    );
+}
